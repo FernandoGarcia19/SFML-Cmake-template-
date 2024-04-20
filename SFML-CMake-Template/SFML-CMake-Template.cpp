6fee74fcb4d2b868a8e603931e54bf9190fd6458
FernandoGarcia19 <fernando.garcia@ucb.edu.bo>
2024-04-19 20:26:00 -0400

    create base project

---
+++ SFML-CMake-Template/SFML-CMake-Template.cpp
@@ -0,0 +1,12 @@
+﻿// SFML-CMake-Template.cpp : Defines the entry point for the application.
+//
+
+#include "SFML-CMake-Template.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
